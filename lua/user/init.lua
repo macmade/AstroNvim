915f9b4aab89f9ef84ba92ef0f540d62cc259ea8
macmade <macmade@xs-labs.com>
2023-03-15 10:18:54 +0100

    user init

---
+++ lua/user/init.lua
@@ -0,0 +1,13 @@
+return {
+  plugins = {
+    {
+      "goolord/alpha-nvim",
+      opts = function(_, opts)
+        opts.section.header.val = {
+          "      XS-Labs      ",
+          "Blood, Sweat & Code",
+        }
+      end,
+    },
+  },
+}
