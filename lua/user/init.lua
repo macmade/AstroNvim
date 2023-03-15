5e74e4d46218b43418812d9f676be54c73c45ccf
macmade <macmade@xs-labs.com>
2023-03-15 21:44:58 +0100

    neotree: showing hidden files.

--- lua/user/init.lua
+++ lua/user/init.lua
@@ -10,5 +10,17 @@ return {
         }
       end,
     },
+    {
+      "nvim-neo-tree/neo-tree.nvim",
+      opts = {
+        filesystem = {
+          filtered_items = {
+            visible = true,
+            hide_dotfiles = false,
+            hide_gitignored = false,
+          }
+        }
+      },
+    },
   },
 }
