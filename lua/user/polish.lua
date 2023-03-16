371aaa7c5ee7155f6078731cc19c25088295e0d8
macmade <macmade@xs-labs.com>
2023-03-16 18:07:35 +0100

    nornu

--- lua/user/polish.lua
+++ lua/user/polish.lua
@@ -1,6 +1,3 @@
 vim.api.nvim_create_autocmd("VimEnter", {
-  command = "set nornu nonu | Neotree toggle",
-})
-vim.api.nvim_create_autocmd("BufEnter", {
-  command = "set rnu nu",
+  command = "set nornu | Neotree toggle",
 })
